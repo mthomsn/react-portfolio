e2543dc34c08136ecb0306abdcf5789cf85a6257
Matt <matthewthomsen99@gmail.com>
2022-12-12 18:03:30 -0600

    refactoring file architecture

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default function About() {
     <section>
       <h1 id="about-h">About me</h1>
       <p id="about-p">
-        I'm an aspiring software developer with a background in customer service and a passion for learning. I'm currently enrolled in the University of Texas Full Stack Coding Bootcamp, where I'm learning the skills necessary to become a full stack web developer. <br></br>
+        I'm an aspiring software engineer with a background in customer service and a passion for learning. I'm currently enrolled in the University of Texas Full Stack Coding Bootcamp, where I'm learning the skills necessary to become a full stack web developer. <br></br>
         I'm excited to leverage my skills in customer service and my passion for learning to build applications that will make a positive impact on the world.
       </p>
     </section>
